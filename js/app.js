9f5c20ef4c8fb134e867ff9ced82b7e589e67197
chou <zhouchaoyi@sohu.com>
2015-10-08 10:20:41 +0800

    m20151008

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 var host="http://112.74.205.42/weiapp/";
 //var host="http://10.12.49.54:8080/assist/";
 //var host="http://192.168.1.89:8080/assist/"; 
+var files_upload_url="http://112.74.205.42/files";
+var files_web=files_upload_url+"/servlet/com.lemon.files.UpLoadify?folder=pstore";  
+var files_path=files_upload_url+"/pstore/";
